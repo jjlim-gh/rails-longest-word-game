d92682e86a6ff663937e71ac94ff1f3055e538d6
Justin Limlengco <jjmlimlengco@gmail.com>
2024-01-05 04:51:34 +0900

    Able to validate if word exists through API

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,6 @@
+require "json"
+require "open-uri"
+
 class GamesController < ApplicationController
   def index
   end
@@ -11,7 +14,22 @@ class GamesController < ApplicationController
     end
   end
 
+  def checker(url)
+    response = URI.open(url).read
+    parsed = JSON.parse(response)
+    return parsed["found"]
+  end
+
   def score
-    @checker = JSON.parse("https://wagon-dictionary.herokuapp.com/#{params[:word]}")
+    word = params[:word]
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    result = checker(url)
+    @display = ""
+
+    if result == true
+      @display = "Congratulations! #{word} is a valid english word!"
+    else
+      @display = "Sorry, #{word} is not a word!"
+    end
   end
 end
